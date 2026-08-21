e6391a9a0a87f87398f22a9f5649447bbed61da7
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-21 07:07:54 +0200

    public import warning fix

--- Sources/RFC 9293/RFC_9293.Segment.swift
+++ Sources/RFC 9293/RFC_9293.Segment.swift
@@ -11,7 +11,7 @@
 // ===----------------------------------------------------------------------===//
 
 public import Binary_Serializable_Primitives
-public import Standard_Library_Extensions
+import Standard_Library_Extensions
 
 extension RFC_9293 {
     /// TCP Segment per RFC 9293
